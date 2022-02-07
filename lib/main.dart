716e2276051055f4dfad85d3bc083910ffbaccc5
Mayankjn25 <mayank.jain.25.1999@gmail.com>
2022-02-07 18:15:19 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAQPzQsC5DYkGxS8y74uNyAoorm2V5N1ys",
+        appId: "1:224133174054:web:7fd7185ec0e485093f67e7",
+        messagingSenderId: "224133174054",
+        projectId: "instagram-clone-a50bd",
+        storageBucket: 'instagram-clone-a50bd.appspot.com'
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
