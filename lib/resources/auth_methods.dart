15f9c333163b5d6391350c1d16c019e44a66e15c
Mayankjn25 <mayank.jain.25.1999@gmail.com>
2022-02-15 12:48:11 +0530

    added user_provider

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -10,6 +10,16 @@ class AuthMethods {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  // get user details
+  Future<model.User> getUserDetails() async {
+    User currentUser = _auth.currentUser!;
+
+    DocumentSnapshot documentSnapshot =
+        await _firestore.collection('users').doc(currentUser.uid).get();
+
+    return model.User.fromSnap(documentSnapshot);
+  }
+
   // Signing Up User
   Future<String> signUpUser({
     required String email,
